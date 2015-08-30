ce50b99032900c70162b1de480408796590a0182
Scott Francis <scott.francis@shopify.com>
2015-08-30 12:15:44 -0400

    vim and colorized things

--- home/.bash/04_aliases.bash
+++ home/.bash/04_aliases.bash
@@ -1,19 +1,20 @@
 #!/bin/bash
 
 if ls --version | grep -q GNU; then
-	alias ls='ls --color=auto'
+  eval `dircolors ~/.dir_colors`
+  alias ls='ls --color=auto'
 else
-	alias ls='ls -G'
+  alias ls='ls -G'
 fi
 
 alias bx='bundle exec'
 
 if [[ -d ~/.chef ]]; then
-# Disable Ruby warnings for knife
-knife() { (cd ~/.chef && RUBYOPT=-W0 bx knife "$@") }
+  # Disable Ruby warnings for knife
+  knife() { (cd ~/.chef && RUBYOPT=-W0 bx knife "$@") }
 fi
 
 if [[ -d ~/vagrant ]]; then
-vssh() { (cd ~/vagrant && vagrant ssh) }
+  vssh() { (cd ~/vagrant && vagrant ssh) }
 fi
 
