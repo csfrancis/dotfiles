41efd591884d73a9c2622d9f7927a111d531d7d9
Scott Francis <scott.francis@shopify.com>
2015-10-24 19:37:34 -0400

    chruby dir

--- home/.bash/ruby.bash
+++ home/.bash/ruby.bash
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-if [[ -r /usr/local/opt/chruby/share/chruby/chruby.sh ]]; then
-	source /usr/local/opt/chruby/share/chruby/chruby.sh
+CHRUBY_DIR=/usr/local/opt/chruby/share/chruby
+if [[ -d /usr/local/share/chruby ]]; then
+  CHRUBY_DIR=/usr/local/share/chruby
+fi
+
+if [[ -r "${CHRUBY_DIR}/chruby.sh" ]]; then
+	source "${CHRUBY_DIR}/chruby.sh"
 	RUBIES=(~/.rubies/*)
 
 	chruby 2.2.3
 
-	source /usr/local/opt/chruby/share/chruby/auto.sh
+	source "${CHRUBY_DIR}/auto.sh"
 fi
 
