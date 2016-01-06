ca89bb0a230d3da7a1f28c274e26c361b159a9ef
Scott Francis <scott.francis@shopify.com>
2016-01-06 10:52:51 -0500

    Use ruby 2.2.3 for knife

--- home/.bash/04_aliases.bash
+++ home/.bash/04_aliases.bash
@@ -11,7 +11,7 @@ alias bx='bundle exec'
 
 if [[ -d ~/.chef ]]; then
   # Disable Ruby warnings for knife
-  knife() { (cd ~/.chef && BUNDLE_GEMFILE=~/.chef/Gemfile bx knife "$@") }
+  knife() { (cd ~/.chef && chruby 2.2.3 && BUNDLE_GEMFILE=~/.chef/Gemfile bx knife "$@") }
 fi
 
 if [[ -d ~/vagrant ]]; then
