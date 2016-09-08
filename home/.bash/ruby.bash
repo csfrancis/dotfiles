4c73308bcec5d4ab4caeca7587c1dfb43e4ee42a
Scott Francis <scott.francis@shopify.com>
2016-09-08 09:28:00 -0400

    Update chruby function

--- home/.bash/ruby.bash
+++ home/.bash/ruby.bash
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-CHRUBY_DIR=/usr/local/opt/chruby/share/chruby
-if [[ -d /usr/local/share/chruby && "${SHOPIFY_DEV_VAGRANT}" != "1" ]]; then
-  CHRUBY_DIR=/usr/local/share/chruby
-fi
-
-if [[ -r "${CHRUBY_DIR}/chruby.sh" ]]; then
-  source "${CHRUBY_DIR}/chruby.sh"
-  RUBIES=(~/.rubies/*)
-
-  if [[ `hostname -s` != "vagrant" ]]; then
-    chruby 2.2.3
+chruby() {
+  CHRUBY_DIR=/usr/local/opt/chruby/share/chruby
+  if [[ -d /usr/local/share/chruby && "${SHOPIFY_DEV_VAGRANT}" != "1" ]]; then
+    CHRUBY_DIR=/usr/local/share/chruby
   fi
 
-  source "${CHRUBY_DIR}/auto.sh"
-fi
+  if [[ -r "${CHRUBY_DIR}/chruby.sh" ]]; then
+    source "${CHRUBY_DIR}/chruby.sh"
+    RUBIES=(~/.rubies/*)
 
+    if [[ `hostname -s` != "vagrant" ]]; then
+      chruby 2.2.3
+    fi
+
+    source "${CHRUBY_DIR}/auto.sh"
+  fi
+}
