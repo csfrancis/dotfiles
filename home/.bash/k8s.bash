35d0ef9081ca4ca331ce7f853b5d63baae5c8314
Scott Francis <scott.francis@shopify.com>
2018-01-26 12:10:48 -0500

    k8s ssh to a pod using the app label

--- home/.bash/k8s.bash
+++ home/.bash/k8s.bash
@@ -6,7 +6,15 @@ kubessh() {
     return 1
   fi
 
-  POD=$1
+  if [[ "$(kubectl get pods "$1" -n "${KUBECTL_NS}" 2>&1 || true)" == Error* ]]; then
+    POD=$(kubectl get pods --selector="app=${1}" -n "${KUBECTL_NS}" | grep ' Running ' | awk '{print $1}')
+    if [ -z "${POD}" ]; then
+      return 1
+    fi
+  else
+    POD=$1
+  fi
+
   NODE=$(kubectl describe "pods/${POD}" -n "${KUBECTL_NS}" | grep 'Node: ' | awk '{ print $2 }' | cut -d'/' -f1)
   ZONE=$(kubectl describe "nodes/${NODE}" | grep failure-domain.beta.kubernetes.io/zone | cut -d'=' -f2)
   gcloud compute ssh "${NODE}" --zone "${ZONE}"
